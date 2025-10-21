d20e1fe7ccf45f15c2e8e8bda4087790142ce9c9
carlosusuriaga <carlosusda@gmail.com>
2025-10-21 15:28:34 -0500

    iniciando

---
+++ src/main/java/ida/pe/Invetario/InvetarioApplication.java
@@ -0,0 +1,13 @@
+package ida.pe.Invetario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvetarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvetarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ida/pe/Invetario/InvetarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ida.pe.Invetario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvetarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
